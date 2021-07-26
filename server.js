de798d06972ad3be85737b8c0a395690974a6431
Jonathan Lauro <jonathanlauro.dev@gmail.com>
2021-07-26 20:28:15 -0300

    vai dar certo

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/visitantes-registros'));
+app.use(express.static(__dirname + '/dist/visitantesRegistros'));
 
 app.get('/*', function(req,res) {
 
 
-    res.sendFile(path.join(__dirname+'/dist/visitantes-registros/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/visitantesRegistros/index.html'));
   });
   
   // Start the app by listening on the default Heroku port
